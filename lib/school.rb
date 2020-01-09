8327bf65074530e4d9c2fcb6cc5b0bfc3bd48577
S. Makai Takori <makait87@gmail.com>
2020-01-09 01:51:51 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -2,20 +2,21 @@ class School
   
 attr_accessor :name, :roster  
 
-def initialize(name, roster = {})
-  @name = name
-  @roster = roster
+  def initialize(name, roster = {})
+    @name = name
+    @roster = roster
+      
+  end  
+
+  def add_student(name, grade)
+    @student_name = name
+    @grade = grade
     
-end  
-
-def add_student(name, grade)
-  @student_name = name
-  @grade = grade
-  
-  roster[grade] ||= []
-  roster[grade] << name 
-
-end
+    # ||= is equivalent to a || a = b; if a returns nil or false then a is set to b
+    
+    roster[grade] ||= []
+    roster[grade] << name 
+  end
 
 
 end  
