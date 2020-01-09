6b3cd5029f1cf9a6fcb17ad2e436758f2db0d152
S. Makai Takori <makait87@gmail.com>
2020-01-09 01:12:06 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,18 @@
-# code here!
+class School
+  
+attr_accessor :name, :roster  
+
+def initialize(name, roster = {})
+  @name = name
+  @roster = roster
+    
+end  
+
+def add_student(name, grade)
+  roster[grade] = []
+  
+
+end
+
+
+end  
